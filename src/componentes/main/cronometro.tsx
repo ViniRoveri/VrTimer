be595682ee694059fc5570eb6ddc62c04651ba23
ViniRoveri <viniciusroverirocha@gmail.com>
2022-10-21 20:04:29 -0300

    Minor Fixes

--- src/componentes/main/cronometro.tsx
+++ src/componentes/main/cronometro.tsx
@@ -1,7 +1,7 @@
 const Cronometro = (props:{displayTimer:number|string,duranteInspecao:string,duranteSolve:string,minTimer:number})=>{
     return(
         <section className={`cronometro ${props.duranteSolve}`}>
-            <span className={`texto ${props.duranteInspecao}`}>Inspeção</span>
+            <span className={`texto naoSelecionavel ${props.duranteInspecao}`}>Inspeção</span>
             <h2 className="naoSelecionavel">{typeof props.displayTimer==='number'?props.displayTimer.toFixed(2):props.displayTimer}</h2>
         </section>
     )
